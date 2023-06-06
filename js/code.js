4d23732447a8382cb034bf67447935183d83d1cb
tylaraynes <70238175+tylaraynes@users.noreply.github.com>
2023-06-05 20:47:29 -0400

    Update code.js

--- js/code.js
+++ js/code.js
@@ -268,7 +268,7 @@ function addContact() {
 
     let jsonPayload = JSON.stringify(tmp);
 
-    let url = urlBase + '/AddContacts.' + extension;
+    let url = urlBase + '/AddContact.' + extension;
 
     let xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
